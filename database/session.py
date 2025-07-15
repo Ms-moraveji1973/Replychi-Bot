403b9680d173624acb6f21adc174f19a18cca17e
Ms-moraveji1973 <ms.moraveji2007@gmail.com>
2025-07-16 00:13:10 +0200

    change database_url

--- database/session.py
+++ database/session.py
@@ -1,7 +1,10 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 import contextlib
 from .models import Base
-
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL, echo=True)
 
