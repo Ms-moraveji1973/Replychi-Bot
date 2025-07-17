29d7c39923d1f367489b1a6ce04a33a05fe65c7e
sadegh <sadegh@srv3377911497.bitcommand.net>
2025-07-17 22:26:17 +0000

    add group and refacto relationship

--- database/models.py
+++ database/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Integer, String, DateTime, BigInteger,UniqueConstraint ,F
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from typing import Optional
+from typing import Optional , List
 
 class Base(DeclarativeBase):
     pass
@@ -16,43 +16,54 @@ class TelegramUser(Base):
     registered_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now)
     total_replies_received: Mapped[int] = mapped_column(Integer, default=0)
     total_replies_sent: Mapped[int] = mapped_column(Integer, default=0)
-    group_id : Mapped[int] = mapped_column(BigInteger, nullable=True)
+
+    group_memberships : Mapped[List['GroupMemberShipRelation']] = relationship(back_populates='user',
+                                                                              cascade='all, delete-orphan')
 
 
-    sent_replies = relationship(
-        'ReplyRelationship',
+class GroupMemberShipRelation(Base):
+    __tablename__ = 'group_relations'
+    id : Mapped[int] = mapped_column(BigInteger, primary_key=True, unique=True, nullable=False)
+    user_id : Mapped[int] = mapped_column(ForeignKey('telegram_users.id',ondelete='CASCADE'),nullable=False)
+    group_id : Mapped[int] = mapped_column(BigInteger, nullable=False)
+
+    __table_args__ = (UniqueConstraint('user_id', 'group_id', name='_user_group_uc'),)
+
+    user : Mapped['TelegramUser'] = relationship(back_populates='group_memberships')
+
+    sent_replies_through_membership : Mapped[List['ReplyRelationship']] = relationship(
         foreign_keys='ReplyRelationship.replier_id',
-        back_populates='replier_user'
+        back_populates='replier_user',
+        cascade='all, delete-orphan'
     )
 
-    received_replies = relationship(
-        'ReplyRelationship',
+    receive_replies_through_membership : Mapped[List['ReplyRelationship']] = relationship(
         foreign_keys='ReplyRelationship.replied_to_id',
-        back_populates='replied_user'
+        back_populates='replied_user',
+        cascade='all, delete-orphan'
     )
 
 
 class ReplyRelationship(Base):
     __tablename__ = 'reply_relations'
-
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, unique=True)
-    replier_id : Mapped[int] = mapped_column(BigInteger, ForeignKey('telegram_users.id'),nullable=False)
-    replied_to_id : Mapped[int] = mapped_column(BigInteger, ForeignKey('telegram_users.id'),nullable=False)
-    reply_count: Mapped[int] = mapped_column(Integer, default=0)
-    group_id : Mapped[int] = mapped_column(BigInteger, nullable=True)
-
+    id: Mapped[int] = mapped_column(BigInteger,primary_key=True,unique=True)
+    replier_id : Mapped[int] = mapped_column(BigInteger, ForeignKey('group_relations.id',ondelete='CASCADE'),nullable=False)
+    replied_to_id : Mapped[int] = mapped_column(BigInteger, ForeignKey('group_relations.id',ondelete='CASCADE'),nullable=False)
+    reply_count : Mapped[int] = mapped_column(Integer, default=0)
 
     __table_args__ = (UniqueConstraint('replier_id', 'replied_to_id', name='_replier_replied_to_uc'),)
 
-    replier_user = relationship(
-        'TelegramUser',
+
+    replier_user : Mapped['GroupMemberShipRelation'] = relationship(
+        'GroupMemberShipRelation',
         foreign_keys=[replier_id],
-        back_populates='sent_replies',
+        back_populates='sent_replies_through_membership',
     )
 
-    replied_user = relationship(
-        'TelegramUser',
+    replied_user : Mapped['GroupMemberShipRelation'] = relationship(
+        'GroupMemberShipRelation',
         foreign_keys=[replied_to_id],
-        back_populates='received_replies',
+        back_populates='receive_replies_through_membership',
     )
+
 
